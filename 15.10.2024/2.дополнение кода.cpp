4d86fb23cdceed89274238e96dc241bac36cc935
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-10-15 17:53:37 +0300

    Create 2.дополнение кода.cpp

---
+++ 15.10.2024/2.дополнение кода.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Example {
+    int mDigit = 0;
+    string mName;
+public:
+    
+    Example() {};
+    ~Example() {};
+};
+
+int main() {}
