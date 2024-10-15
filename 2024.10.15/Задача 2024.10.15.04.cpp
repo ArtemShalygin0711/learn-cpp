fa1b8148b2c289997495a64ba4ed15abd3a8c83c
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-10-15 19:28:33 +0300

    task4

---
+++ 2024.10.15/Задача 2024.10.15.04.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Print(double value) {
+    cout << value;
+}
+
+string Print(string value) {
+    cout << value;
+}
+
+int Print(int value) {
+    cout << value;
+}
+
+char Print(char value) {
+    cout << value;
+}
