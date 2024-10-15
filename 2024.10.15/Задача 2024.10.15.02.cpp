bcbe10654b48583a8d271b598c4b4c8d308cb623
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-10-15 18:40:39 +0300

    update task2

--- 2024.10.15/Задача 2024.10.15.02.cpp
+++ 2024.10.15/Задача 2024.10.15.02.cpp
@@ -7,7 +7,10 @@ class Example {
     int mDigit = 0;
     string mName;
 public:
-    
+    void GetDigit(int Digit) { mDigit = Digit; }
+    int GetDigit() { return mDigit; }
+    void GetName(string Name) { mName = Name; }
+    string GetName() { return mName; }
     Example(int Digit, string Name):mDigit(Digit), mName(Name) {};
     ~Example() {};
 };
