f6b11c608d2098bb1321bd5fbf4c66cfcb9cc4d7
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-10-17 18:49:13 +0300

    comments

--- 2024.10.17/number.cpp
+++ 2024.10.17/number.cpp
@@ -1,8 +1,9 @@
+//1-2 строки: добавление библиотек
 #include <iostream>
 #include <string>
 
 using namespace std;
-
+//7-15 строки: создание класса Number и описание его метода, сеттера и геттера и конструктора и деструктораю. Класс хранит в стебе переменнную mValue типа int
 class Number {
 	int mValue = 0;
 public:
@@ -12,15 +13,16 @@ public:
 	Number() {};
 	~Number() {};
 };
-
+//метод класса Number
 void Number::Print() {
 	cout << mValue;
 }
-
+// функция Print, которая принимает на вход переменную типа класса Number и запускает метод класса Number
 void Print(Number n) {
 	n.Print();
 }
 
+//функция main
 int main() {
 	Number numb = Number();
 	Print(numb);
