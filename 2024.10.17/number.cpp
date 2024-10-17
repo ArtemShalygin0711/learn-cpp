e57e4e892282388f3207c3d406b7aa9c775c91c9
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-10-17 17:58:19 +0300

    task2

---
+++ 2024.10.17/number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Number {
+	int mValue;
+public:
+	void Numb(int Value) { mValue = Value; }
+	int Numb() { return mValue; }
+	Number() {};
+	~Number() {};
+};
+
+int main() {
+
+}
