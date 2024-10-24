dce5b9d70cd69acd39580b9daafeca7a55bfc9a2
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-10-24 20:07:35 +0300

    class human comlete, other no

--- 2024.10.22/FlatHouse.cpp
+++ 2024.10.22/FlatHouse.cpp
@@ -1,80 +1,132 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-class Human{
-	string human;
+class Human {
+	string mName;
+	string mSurname;
+	string mPatronymic;
 public:
-	void HUman(string HumaN) { human = HumaN; }
-	string HUman() { return human; }
-	Human(string huMan = ""):human(huMan) {};
-	Human(const Human &h);
+	// getter & setter
+	void SetName(string value) { mName = value; }
+	string GetName() { return mName; }
+	void SetSurname(string value) { mSurname = value; }
+	string GetSurname() { return mSurname; }
+	void SetPatronymic(string value) { mPatronymic = value; }
+	string GetPatronymic() { return mPatronymic; }
+	string GetString();
+
+	// ctor & dtor
+	Human(string name, string surname, string patronymic);
+	Human(const Human& h);
 	~Human() {};
-
-
 };
 
-Human::Human(const Human& h) {
-	human = h.human;
+Human::Human(string name = "", string surname = "", string patronymic = "") :
+	mName(name), mSurname(surname), mPatronymic(patronymic) {};
+Human::Human(const Human& h) : mName(h.mName), mSurname(h.mSurname), mPatronymic(h.mPatronymic) {};
 
+string Human::GetString()
+{
+	return mSurname + " " + mName + " " + mPatronymic + "\n";
+}
 
-};
-
-
-class Flat{
-	vector<Human> humans;
+class Flat {
+	vector<Human> Humans;
 	int NumberOfFlat;
 public:
 
-	void AppendHuman(string human);
+	void AppendHuman(string Human);
 	void NumberFlats(int numb) { NumberOfFlat = numb; }
 	int NumberFlats() { return NumberOfFlat; }
-	Human GetString();
-	Flat(int numb = 0):NumberOfFlat(numb) {};
+	string GetString();
+	void Print();
+	void AppendHuman(string name, string surname, string patronymic);
+	Flat(int numb = 0) :NumberOfFlat(numb) {};
 	//Flat(const Flat &h);
 	~Flat() {};
 
-	
+
 };
 
+void Flat::Print() {
+	cout << GetString();
 
-Human Flat::GetString() {
-	for (int i = 0; i < humans.size(); i++) {
-		return humans[i];
+}
+
+string Flat::GetString() {
+	string result = "Flat: " + to_string(NumberOfFlat + 1) + "\n";
+	for (int i = 0; i < Humans.size(); i++) {
+		result += Humans[i].GetString();
 	}
+	
+	return result;
+	
 }
 
 
-class House{
-	vector<Flat> AmountOfFlat;
+
+class House {
+	vector<Flat> Flats;
+	int numberHouse;
 public:
+	void SetNumberHouse(int numberhouse) { numberHouse = numberhouse; }
+	int GetNumberHouse() { return numberHouse; }
+	void Print();
+
 	void AppendNumberOfFlats(int number);
+	string GetString();
+
 
 	House() {};
 	House(const House& m);
 	~House() {};
 };
 
+string House::GetString() {
+	string result = "Flat: " + to_string(numberHouse) + "\n";
+	for (int i = 0; i < Flats.size(); i++) {
+		result += Flats[i].GetString();
+	}
 
-void Flat::AppendHuman(string human) {
-	Human h = Human(human);
-	humans.push_back(h);
+	return result;
+
+}
+
+
+void House::Print() {
+	cout << GetString();
+}
+
+
+
+
+
+
+
+void Flat::AppendHuman(string name, string surname, string patronymic) {
+	Human h = Human(name, surname, patronymic);
+	Humans.push_back(h);
 }
 
 void House::AppendNumberOfFlats(int number) {
 	Flat n = Flat(number);
-	AmountOfFlat.push_back(n);
+	Flats.push_back(n);
 }
 
 
 int main() {
-	Human h = Human();
-	h.HUman("tom");
-	Flat f = Flat();
-	f.AppendHuman("tom");
 	House n = House();
 	n.AppendNumberOfFlats(1);
-	f.GetString();
+	Human h = Human("tom", "cruse", "abc");
+	Flat f = Flat(4);
+	f.AppendHuman("tom", "cruse", "abc");
+	f.Print();
+	f.AppendHuman("tom", "cruse", "abc");
+	f.Print();
+	n.AppendNumberOfFlats(4);
+	n.Print();
 
 }
