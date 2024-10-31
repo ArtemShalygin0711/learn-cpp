48ed1a215bd9df18923c48ebf0cf8a29b65eead0
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-10-31 19:37:40 +0300

    with explicit

---
+++ 2024.10.31/task4.2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class FloatNumber {
+	float mValue = 0.0;
+public:
+	void Numb(float Value) { mValue = Value; }
+	float Numb() { return mValue; }
+	void Print();
+	FloatNumber() {};
+	explicit FloatNumber(float value) :mValue(value) {};
+	~FloatNumber() {};
+};
+
+void FloatNumber::Print() {
+	cout << mValue;
+}
+
+void Print(FloatNumber n) {
+	n.Print();
+}
+
+int main()
+{
+	FloatNumber numb = FloatNumber();
+	float Value = 2.5;
+	numb.Numb(Value);
+	Print(numb);
