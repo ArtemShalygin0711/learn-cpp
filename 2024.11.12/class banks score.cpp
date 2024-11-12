d0d8dfde238d70c3cea168999efb9a0520031eef
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-11-12 18:38:19 +0300

    inheritance

--- 2024.11.12/class banks score.cpp
+++ 2024.11.12/class banks score.cpp
@@ -9,6 +9,7 @@ class BanksAccount {
 	string titleAccount;
 	float balance;
 public:
+
 	void SetNumberAccount(int numbAccount) { numberAccount = numbAccount; }
 	int GetNumberAccount() { return numberAccount; }
 	void SetTitleBank(string secondTitleBank) { titleBank = secondTitleBank; }
@@ -21,61 +22,28 @@ public:
 	~BanksAccount() {};
 };
 
-class DebetCard {
-	int numberAccount;
+class DebetCard:public BanksAccount {
 	int numberCard;
 	string expirationDate;
 	int cvcCode;
-	string titleBank;
-	string titleAccount;
-	float balanceAccount;
 public:
-	void SetNumberAccount(int numbAccount) { numberAccount = numbAccount; }
-	int GetNumberAccount() { return numberAccount; }
-	void SetTitleBank(string secondTitleBank) { titleBank = secondTitleBank; }
-	string GetTitleBank() { return titleBank; }
-	void SetTitleAccount(string secondTitleAccount) { titleAccount = secondTitleAccount; }
-	string GetTitleAccount() { return titleAccount; }
-	void SetBalanceAccount(float balanced) { balanceAccount = balanced; }
-	float GetBalanceAccount() { return balanceAccount; }
 	void SetNumberCard(int numbCard) { numberCard = numbCard; }
 	int GetNumberCard() { return numberCard; }
 	void SetExpirationDate(string expDate) { expirationDate = expDate; }
 	string GetExpirationDate() { return expirationDate; }
 	void SetCvcCode(int cvc) { cvcCode = cvc; }
 	int GetCvcCode() { return cvcCode; }
-	DebetCard(int numbAccount = 0, int numbCard = 0, string expDate = " ", int cvc = 0, string titlebank = " ", string titleaccount = " ", float balance = 0.0) : numberAccount(numbAccount), numberCard(numbCard), expirationDate(expDate), cvcCode(cvc), titleBank(titlebank), titleAccount(titleaccount), balanceAccount(balance){};
+	DebetCard(int numbCard = 0, string expDate = " ", int cvc = 0) :  numberCard(numbCard), expirationDate(expDate), cvcCode(cvc){};
 	~DebetCard() {};
 };
 
 
-class CreditCard {
-	int numberAccount;
-	int numberCard;
-	string expirationDate;
-	int cvcCode;
-	string titleBank;
-	string titleAccount;
-	float balanceAccount;
+class CreditCard : public DebetCard {
 	float creditLimit;
 public:
-	void SetNumberAccount(int numbAccount) { numberAccount = numbAccount; }
-	int GetNumberAccount() { return numberAccount; }
-	void SetTitleBank(string secondTitleBank) { titleBank = secondTitleBank; }
-	string GetTitleBank() { return titleBank; }
-	void SetTitleAccount(string secondTitleAccount) { titleAccount = secondTitleAccount; }
-	string GetTitleAccount() { return titleAccount; }
-	void SetBalanceAccount(float balanced) { balanceAccount = balanced; }
-	float GetBalanceAccount() { return balanceAccount; }
-	void SetNumberCard(int numbCard) { numberCard = numbCard; }
-	int GetNumberCard() { return numberCard; }
-	void SetExpirationDate(string expDate) { expirationDate = expDate; }
-	string GetExpirationDate() { return expirationDate; }
-	void SetCvcCode(int cvc) { cvcCode = cvc; }
-	int GetCvcCode() { return cvcCode; }
 	void SetCreditLimit(float creditlimit) { creditLimit = creditlimit; }
 	float GetCreditLimit() { return creditLimit; }
-	CreditCard(int numbAccount = 0, int numbCard = 0, string expDate = " ", int cvc = 0, string titlebank = " ", string titleaccount = " ", float balance = 0.0, float creditlimit = 0.0) : numberAccount(numbAccount), numberCard(numbCard), expirationDate(expDate), cvcCode(cvc), titleBank(titlebank), titleAccount(titleaccount), balanceAccount(balance), creditLimit(creditlimit) {};
+	CreditCard(float creditlimit = 0.0) :creditLimit(creditlimit) {};
 	~CreditCard() {};
 };
 
