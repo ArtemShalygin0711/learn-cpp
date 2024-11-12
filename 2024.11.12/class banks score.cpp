0d2a1820146bbe1535bbca56a5a36780c369aa84
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-11-12 18:21:38 +0300

    debet card

--- 2024.11.12/class banks score.cpp
+++ 2024.11.12/class banks score.cpp
@@ -21,6 +21,37 @@ public:
 	~BanksAccount() {};
 };
 
+class DebetCard {
+	int numberAccount;
+	int numberCard;
+	string expirationDate;
+	int cvcCode;
+	string titleBank;
+	string titleAccount;
+	float balanceAccount;
+public:
+	void SetNumberAccount(int numbAccount) { numberAccount = numbAccount; }
+	int GetNumberAccount() { return numberAccount; }
+	void SetTitleBank(string secondTitleBank) { titleBank = secondTitleBank; }
+	string GetTitleBank() { return titleBank; }
+	void SetTitleAccount(string secondTitleAccount) { titleAccount = secondTitleAccount; }
+	string GetTitleAccount() { return titleAccount; }
+	void SetBalanceAccount(float balanced) { balanceAccount = balanced; }
+	float GetBalanceAccount() { return balanceAccount; }
+	void SetNumberCard(int numbCard) { numberCard = numbCard; }
+	int GetNumberCard() { return numberCard; }
+	void SetExpirationDate(string expDate) { expirationDate = expDate; }
+	string GetExpirationDate() { return expirationDate; }
+	void SetCvcCode(int cvc) { cvcCode = cvc; }
+	int GetCvcCode() { return cvcCode; }
+	DebetCard(int numbAccount = 0, int numbCard = 0, string expDate = " ", int cvc = 0, string titlebank = " ", string titleaccount = " ", float balance = 0.0) : numberAccount(numbAccount), numberCard(numbCard), expirationDate(expDate), cvcCode(cvc), titleBank(titlebank), titleAccount(titleaccount), balanceAccount(balance){};
+	~DebetCard() {};
+};
+
+
+
+
+
 int main() {
 
 }
