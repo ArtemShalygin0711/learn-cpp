a302853a443836b28dc4212e475a9bee03fc988b
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2024-11-14 19:59:41 +0300

    Update another world.cpp

--- 2024.11.12/another world.cpp
+++ 2024.11.12/another world.cpp
@@ -3,32 +3,19 @@
 #include<vector>
 #include<ctime>
 using namespace std;
-
 string RandFruitsNames[10] = { "tom", "scott", "mark", "fedor", "alexandr", "alex", "borya", "fedya", "patrick", "gerodot" };
 string RandFruitsSurnames[10] = { "Ivanov", "Petrov", "Smirnov", "Kuznetsov", "Popov", "Sidorov", "Morozov", "Vasilyev", "Fedorov", "Novikov" };
 string RandFruitsPatronymic[10] = { "Ivanovich", "Petrov", "Smirnovich", "Kuznetsov", "Popovich", "Sidorov", "Morozovich", "Vasilyev", "Fedorovich", "Novikov" };
-
-string GetRandomName() {
-	string randomName = RandFruitsNames[rand() % 10];
-	return randomName;
-}
-
-string GetRandomSurname() {
-	string randomSurname = RandFruitsSurnames[rand() % 10];
-	return randomSurname;
-}
-
-string GetRandomPatronymic() {
-	string randomPatronymic = RandFruitsPatronymic[rand() % 10];
-	return randomPatronymic;
-}
-
+string GetRandomName() {return RandFruitsNames[rand() % 10];}
+string GetRandomSurname() {return RandFruitsSurnames[rand() % 10];}
+string GetRandomPatronymic() {return RandFruitsPatronymic[rand() % 10];}
 class Fruit {
 protected:
 	string mName;
 	string mSurname;
 	string mPatronymic;
 	int mAge;
+	string percent;
 	vector<Fruit> Fruits;
 public:
 	void SetName(string name) { mName = name; }
@@ -41,39 +28,37 @@ public:
 	int GetAge() { return  mAge; }
 	void Print();
 	string GetString();
-	Fruit(string name = "", string surname = "", string patronymic = "", int age = 0) :mName(name), mSurname(surname), mPatronymic(patronymic), mAge(age) {};
+	Fruit(string name = "", string surname = "", string patronymic = "", int age = 0, string percent = "") :mName(name), mSurname(surname), mPatronymic(patronymic), mAge(age), percent(percent) {};
+	Fruit(const Fruit &f) :mName(f.mName), mSurname(f.mSurname), mPatronymic(f.mPatronymic), mAge(f.mAge), percent(f.percent) {};
 	~Fruit() {};
 };
 
-string Fruit::GetString() {
-	SetName(GetRandomName());
-	SetSurname(GetRandomSurname());
-	SetPatronymic(GetRandomPatronymic());
-	return mSurname + " " + mName + " " + mPatronymic + "\n";
-}
+string Fruit::GetString() {return mSurname + " " + mName + " " + mPatronymic + " " + to_string(mAge) + "\n"; }
 
 class Orange : public Fruit {
 	int oOrangesdegree;
 public:
 	void SetOrangesDegree(int percent) { oOrangesdegree = percent; }
 	int GetOrangesDegree() { return oOrangesdegree; }
-	Orange(int percent) :oOrangesdegree(percent) {};
+	Orange(int percent) :oOrangesdegree(percent) { AppendOranges(); };
+	Orange(const Orange& o):oOrangesdegree(o.oOrangesdegree){};
 	void AppendOranges();
-	Fruit GetString();
+	string GetString();
 	void Print();
 	~Orange() {};
 };
 
-Fruit Orange::GetString() {
-	for (int i = 0; i < 10; i++) {
-		Fruit fruit = GetString();
-		return Fruits[i];
+string Orange::GetString() {
+	string result;
+	for (int i = 0; i < Fruits.size(); i++) {
+		result += Fruits[i].GetString();
 	}
+	return result;
 }
 
 void Orange::AppendOranges() {
 	for (int i = 0; i < 10; i++) {
-		Fruit fruit = Fruit();
+		Fruit fruit = Fruit(GetRandomName(), GetRandomSurname(), GetRandomPatronymic(), rand()%100 + 1, "on he is  " + to_string(rand() % 100 + 1) + " percent of an orange");
 		Fruits.push_back(fruit);
 	}
 };
@@ -83,28 +68,52 @@ class Pinaple : public Fruit {
 public:
 	void SetPinaplessDegree(int percent) { oPinaplesdegree = percent; }
 	int GetPinaplessDegree() { return oPinaplesdegree; }
+	void AppendPinaples();
+	string GetString();
 	Pinaple(int percent) :oPinaplesdegree(percent) {};
 	~Pinaple() {};
 };
+
+
+void Pinaple::AppendPinaples() {
+	for (int i = 0; i < 10; i++) {
+		Fruit fruit = Fruit(GetRandomName(), GetRandomSurname(), GetRandomPatronymic(), rand() % 100 + 1, "on he is  " + to_string(rand() % 100 + 1) + " percent of an pinapple");
+		Fruits.push_back(fruit);
+	}
+}
 
 class Apple : public Fruit {
 	int oApplesdegree;
 public:
 	void SetPinaplessDegree(int percent) { oApplesdegree = percent; }
 	int GetPinaplessDegree() { return oApplesdegree; }
+	void AppendApples();
 	Apple(int percent) :oApplesdegree(percent) {};
 	~Apple() {};
 };
 
+void Apple::AppendApples() {
+	for (int i = 0; i < 10; i++) {
+		Fruit fruit = Fruit(GetRandomName(), GetRandomSurname(), GetRandomPatronymic(), rand() % 100 + 1," on he is  " + to_string(rand() % 100 + 1) + " percent of an apple");
+		Fruits.push_back(fruit);
+	}
+}
 
 void Orange::Print() {
-	Fruit f = Fruit();
-	cout << f.GetString();
+	cout << GetString();
 }
 
 
+void Fruit::Print() {
+	string result;
+	for (int i = 0; i < 30; i++) {
+		result += Fruits[i].GetString();
+	}
+	cout << result;
+}
+
 int main() {
 	srand(time(0));
-	Orange orange = Orange(20);
-	orange.Print();
+	Fruit f = Fruit();
+	f.Print();
 }
