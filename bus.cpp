81eebe277ed3608c08ffdff80c78b156733e03f3
ArtemShalygin0711 <artem.shalygin.00inbox.ru@gmail.com>
2025-02-20 18:47:19 +0300

    Update bus.cpp

--- bus.cpp
+++ bus.cpp
@@ -1,3 +1,4 @@
+//библиотеки
 #include <iostream>
 #include <queue>
 #include <map>
@@ -6,18 +7,28 @@
 #include <deque>
 #include <vector>
 using namespace std;
-
+//класс автобус
 class Bus {
 private:
+	//структуры класса
+	//очередь, которая сохраняет автобусы в приоритетном порядке в формате нмер автобуса - число пассажиров
 	priority_queue<pair<int, int>> pd;
+	//сохраняет количество запусков маршрутов и номера этих маршрутов
 	map<int, int> m;
+	//сохраняет общее количество перевезенных пассажиров на каждом автобусе
 	map<int, int> amountpassengers;
+	//сохраняет номера всех маршрутов
 	set<int> s;
+	//очередь которая в приоритетном порядке сохраняет автобус для дальнейшей отправки на рейс
 	queue<int> onreis;
+	//очередь которая сохраняет автобусы для ожидания отправки на рейс
 	deque<int> conservate;
+	//сохраняет историю завершенных рейсов
 	deque<int> history;
 public:
+	//функция для добавления автобусов
 	void add_bus(int route, int passengers, int capacity) {
+		//условие проверяет загруженность автобуса
 		if (passengers > capacity * 0.9) {
 			pd.push({ passengers*10,route });
 		}
@@ -30,20 +41,27 @@ public:
 			s.insert(route);
 		}
 	}
+	//отправка автобуса в рейс
 	void dispatch_bus() {
 		onreis.push(pd.top().second);
+		//условие на проверку загруженности автобуса для добавления в лист ожидания
 		if (pd.top().second < 7) {
 			conservate.push_back(pd.top().second);
 		}
 	}
+	//завершение рейса
 	void complete_trip() {
 		m[onreis.front()] += 1;
+		//добавление завершенного рейса в историю
 		history.push_back(onreis.front());
 		onreis.pop();
 	}
+	//галя отмена
 	void cancel_route(int route) {
 		m.erase(route);
+		//перевод структуры данных set в вектор для далььнейшей итерации
 		vector<int> v(s.begin(), s.end());
+		//цикл на нахождение нужного рейса для его отмены
 		for (int i = 0; i < s.size(); i++) {
 			if (v[i] == route) {
 				s.erase(i);
@@ -52,12 +70,14 @@ public:
 		}
 		priority_queue<pair<int, int>> pd2;
 		while (!pd.empty()) {
+			//обратный перевод вектора в set
 			if (pd.top().second != route) {
 				pd2.push(pd.top());
 				pd.pop();
 			}
 		}
 		pd = pd2;
+		//цикл для удаления автобуа из истории
 		for (int i = 0; i < history.size(); i++) {
 			if (history[i] == route) {
 				history.erase(remove(history.begin(), history.end(), i), history.end());
@@ -65,9 +85,11 @@ public:
 
 		}
 	}
+	//получение самых популярных маршрутов
 	void get_most_popular_routes() {
 		int m1=0;
 		int key;
+		//цикл для получения топа рейсов
 		for (auto c : m) {
 			if (c.second > m1) {
 				m1 = c.second;
@@ -76,10 +98,12 @@ public:
 		}
 		cout << "top one: " << key;
 	}
+	//статистика
 	void get_route_statistics(int route) {
 		cout << "\namount of trips " << m[route] << endl;
 		cout << "middle " << amountpassengers[route] / m[route] << endl;
 	}
+	//функция для производства, показывает, что загрузили в прогу и на чем держита благополучие и благосостояние моей нервной системы
 	void print() {
 		cout << "pd\n";
 		while (!pd.empty()) {
@@ -116,6 +140,7 @@ public:
 		}
 	}
 };
+//функция мэйн, она все запускает, запускает эту колесницу смерти
 int main() {
 	Bus bus = Bus();
 	bus.add_bus(122, 30, 40);
